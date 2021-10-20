60ebe33678282d51d29af405d30f9d5a2db793a3
lautarovollmer <lautarovollmer0@gmail.com>
2021-10-20 18:19:08 -0300

    my second change

---
+++ 02-JS-I/homework/definiciones.js
@@ -0,0 +1,21 @@
+/* Variable: Una variable es una forma de almacenar el valor de algo para usar más tarde. 
+Para crear una variable en JavaScript se utiliza la palabra var seguida de un espacio y el nombre de la variable, ademas de eso, podemos asignarle un valor con el sigo =
+ Existen 3 formas de declarar una variable:
+     var nombre = 'Juan';  
+     let apellido = 'Perez';
+     const comidafavorita = 'Pizza';
+*/
+
+// Strings: Las strings son bloques de texto, siempre se definirán entre comillas, ya sea simple o doble. Cualquier texto entre comillas es una cadena o string.
+// var nombrePerro = 'firulais';
+
+// Funciones: Són tipos particulares de Objetos, llamados objetos invocables, por lo que tienen las mismas propiedades que cualquier objeto.
+// Una función comenzará con la palabra clave function, esto le dice a lo que sea que esté ejecutando tu programa que lo que sigue es una función y que debe tratarse como tal. Después de eso viene el nombre de la función. Luego viene un paréntesis abierto y uno cercano. Y finalmente, abra y cierre los corchetes. Entre estos corchetes es donde irá todo nuestro código a ejecutar.
+
+function logHola() {
+    console.log('hola!');
+}
+
+logHola();
+
+
